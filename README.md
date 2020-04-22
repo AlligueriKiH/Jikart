37616bb0b1a5edb2682f39b18e2f51eb192ae98e
ale183 <ale183.dev@gmail.com>
2020-04-22 18:23:23 +0200

    New example.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Jikart
- 
+
+## Example
+```dart
+import 'package:jikart/jikart.dart';
+
+void main() async{
+  var jikart = Jikart();
+
+  var anime_id = 37786; // https://myanimelist.net/anime/37786/
+
+  var anime = await jikart.anime.getAnime(anime_id);
+  var episodes = await jikart.anime.getEpisodes(anime_id);
+  var stats = await jikart.anime.getStats(anime_id);
+
+  print('${anime.title}');
+  print('Score: ${anime.score}\n');
+
+  print('Episodes:');
+  episodes?.forEach((f) => print('${f.episode_id} ${f.title}'));
+
+  print('\nScores:');
+  stats.scores?.forEach((k, v) => print('${k}: ${v.votes} (${v.percentage}%)'));
+
+}
+```
--- example/Jikart_example.dart
+++ example/Jikart_example.dart
@@ -1,7 +1,21 @@
 import 'package:jikart/jikart.dart';
 
-void main() {
+void main() async{
   var jikart = Jikart();
-  jikart.anime.getAnime(1)
-      .then((result) => print('${result['title']} - ${result['episodes']} episodes'));
+
+  var anime_id = 37786; // https://myanimelist.net/anime/37786/
+
+  var anime = await jikart.anime.getAnime(anime_id);
+  var episodes = await jikart.anime.getEpisodes(anime_id);
+  var stats = await jikart.anime.getStats(anime_id);
+
+  print('${anime.title}');
+  print('Score: ${anime.score}\n');
+
+  print('Episodes:');
+  episodes?.forEach((f) => print('${f.episode_id} ${f.title}'));
+
+  print('\nScores:');
+  stats.scores?.forEach((k, v) => print('${k}: ${v.votes} (${v.percentage}%)'));
+
 }
